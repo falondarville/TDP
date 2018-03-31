2846036cb20386f736fae84207cff503fb3b423d
Falon Darville <falondarville@gmail.com>
2018-03-30 19:32:49 -0700

    add play on page section

--- logic.js
+++ logic.js
@@ -1,9 +1,9 @@
 $(document).ready(function() {
 
 // About page vertical menu
-$('#myTab a').on('click', function (event) {
+$("#myTab a").on("click", function (event) {
   event.preventDefault();
-  $(this).tab('show');
+  $(this).tab("show");
 });
 
 $('#about a[href="#about"]').tab('show');
@@ -11,14 +11,11 @@ $('#where a[href="#where"]').tab('show');
 $('#faq a[href="#faq"]').tab('show');
 $('#contact a[href="#contact"]').tab('show');
 
-// topics sorting
-$(".menu-topics").click(function(){
-
-	$("#currentTopic").empty();
-	var value = $(this).attr("data-filter");
-
-	// code what data to show given the topic chosen
-
+$("#collapse-button").on("click", function() {
+	$("#collapsed-area").removeClass("collapse");
+    $('html, body').animate({
+        scrollTop: $("#collapsed-area").offset().top
+    }, 1000);
 });
 
 });
